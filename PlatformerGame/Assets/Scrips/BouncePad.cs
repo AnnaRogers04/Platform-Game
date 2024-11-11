51114654c2cc5d014593c7ab6746b363646ef7fe
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-11-11 15:52:36 +0000

    BouncePadDone

---
+++ PlatformerGame/Assets/Scrips/BouncePad.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public float bounceForce = 25f;
+    private void OnTriggerEnter(Collider other)
+    {
+        var rb = other.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            Debug.Log("PLEASE WORK!");
+            rb.AddForce(Vector3.up * bounceForce);
+        }
+
+    }
+}

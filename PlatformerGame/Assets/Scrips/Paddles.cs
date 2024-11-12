81611c142a97bde9ccc76a9b0ce89ecb545379bf
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-11-12 15:19:49 +0000

    .

---
+++ PlatformerGame/Assets/Scrips/Paddles.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddles : MonoBehaviour
+{
+    // max rotation value
+    // min rotation value
+    // rotationspeed
+    private void Update()
+    {
+        // if the paddle button is pressed & object rotation != max rotation
+            // object rotation += rotationspeed * deltaTime;
+        // if the paddle button is released & object rotation != min rotation
+            // object rotation-= rotationspeed * deltaTime;
+    }
+}

c876be53d9d888a9ed331c3b77a03c7592323c57
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-11-18 16:58:34 +0000

    attempting to rotate

--- PlatformerGame/Assets/Scrips/Paddles.cs
+++ PlatformerGame/Assets/Scrips/Paddles.cs
@@ -1,17 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddles : MonoBehaviour
 {
-    // max rotation value
-    // min rotation value
-    // rotationspeed
+    [SerializeField] private GameObject pivotObject;
+    [SerializeField] private bool Rotation;
+    [SerializeField] private float min = 0f;
+    [SerializeField] private float max = 90f;
+    private float temp;
+    private bool RoatateAround;
     private void Update()
     {
-        // if the paddle button is pressed & object rotation != max rotation
-            // object rotation += rotationspeed * deltaTime;
-        // if the paddle button is released & object rotation != min rotation
-            // object rotation-= rotationspeed * deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && RoatateAround ==false) //just to get input
+        {
+            RoatateAround = true;
+        }
+
+
+        if (RoatateAround)
+        {
+            if (Rotation) //should rotate left
+            {
+                gameObject.transform.RotateAround(pivotObject.transform.position, Vector3.left, Mathf.Lerp(min, max, temp ));   
+            }
+            else
+            {
+                gameObject.transform.RotateAround(pivotObject.transform.position, Vector3.right, Mathf.Lerp(min, max, temp));
+            }
+            
+            temp += 0.5f * Time.deltaTime;
+        } //after you complete rotation reset roatatearound to false;
+
+        if (temp >= max)
+        {
+            gameObject.transform.RotateAround(pivotObject.transform.position, Vector3.left, Mathf.Lerp(max, min, temp )); 
+        }
+        else
+        {
+            gameObject.transform.RotateAround(pivotObject.transform.position, Vector3.right, Mathf.Lerp(max, min, temp));
+        }    
     }
 }
+    

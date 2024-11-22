61682bd2b86e70d152997515efba6de088ff20f3
AnnaRogers04 <2315276@students.ucreative.ac.uk>
2024-11-22 13:03:59 +0000

    physics done

--- PlatformerGame/Assets/Scrips/Paddle.cs
+++ PlatformerGame/Assets/Scrips/Paddle.cs
@@ -4,32 +4,57 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    public float restPos = 0f, pressedPos = 45f, hitStrength = 1000f, flipperDamper = 150f;
-    [SerializeField]private HingeJoint _hinge;
-    // Start is called before the first frame update
-    private void Start()
+    public float forceStrength = 100f; // Force applied to the paddle
+    public float returnForceStrength = 50f; // Force for returning to min rotation
+    public float minRotation = -30f; // Minimum allowable rotation on the X-axis
+    public float maxRotation = 30f;  // Maximum allowable rotation on the X-axis
+    private Rigidbody _rigidbody;
+
+    void Start()
     {
-        _hinge = GetComponent<HingeJoint>();
-        _hinge.useSpring = true;
+        // Get the Rigidbody component
+        _rigidbody = GetComponent<Rigidbody>();
+
+        // Freeze unnecessary axes to constrain paddle movement
+        _rigidbody.constraints = RigidbodyConstraints.FreezePositionZ |
+                                 RigidbodyConstraints.FreezeRotationZ |
+                                 RigidbodyConstraints.FreezeRotationY |
+                                 RigidbodyConstraints.FreezePositionX |
+                                 RigidbodyConstraints.FreezePositionY;
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        JointSpring spring = new JointSpring();
-        spring.spring = hitStrength;
-        spring.damper = flipperDamper;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Check if the space bar is pressed
+        if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed Space");
-            spring.targetPosition = pressedPos;
+            ApplyForceTowards(maxRotation);
         }
         else
         {
-            spring.targetPosition = restPos;
+            ApplyForceTowards(minRotation);
         }
-        _hinge.spring = spring;
-        _hinge.useLimits = true;
+    }
+
+    void ApplyForceTowards(float targetRotation)
+    {
+        // Get the current rotation of the paddle in local space
+        float currentRotation = transform.localEulerAngles.x;
+        if (currentRotation > 180f) currentRotation -= 360f; // Convert to a range of -180 to 180
+
+        // Determine the direction to apply force
+        float direction = targetRotation > currentRotation ? 1f : -1f;
+
+        // Calculate the force direction along the X-axis
+        Vector3 torque = transform.right * direction * (forceStrength * 1000000000f);
+
+        // Apply torque to the rigidbody
+        _rigidbody.AddTorque(torque * 10000f);
+
+        // Stop applying force if close to the target rotation
+        if (Mathf.Abs(targetRotation - currentRotation) < 1f)
+        {
+            _rigidbody.angularVelocity = Vector3.zero;
+        }
     }
 }
